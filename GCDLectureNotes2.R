ca154a7ee4034e755c42e8e216e61e771e36c5c0
BJKill <brandon.j.kill@gmail.com>
2020-07-16 17:21:55 -0400

    First few lecture slides. Installed MySQR.

--- GCDLectureNotes2.R
+++ GCDLectureNotes2.R
@@ -1,8 +1,18 @@
 #### Getting and Cleaning Data - Week 2 ####
 
+### MySQL ("my sequal")
+# - Free and widely used open source database software
+# - Widely used in internet based applications
+# - Data are structured in
+#       - Databases
+#       - Tables within databases
+#       - Fields within tables
+# - Each row is called a record
+# httb://en.wikipedia.org/wiki/MySQL
+# hppt://www.mysql.com/
 
-
-
+## Example Structure
+# http://dev.mysql.com/doc/employee/en/sakila-structure.html
 
 
 
